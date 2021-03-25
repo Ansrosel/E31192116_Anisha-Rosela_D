f6e65a92c04b1caa27a9da118d266c2c7a783ba0
Ansrosel <anisharosela@gmail.com>
2021-03-25 17:32:38 +0700

    first commit

---
+++ minggu3/E31192116_Tugas3_Activity/app/src/main/java/com/anisha/e31192116_tugas3_activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anisha.e31192116_tugas3_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
